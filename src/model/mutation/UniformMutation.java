d0dfa744869e642b8ad004f71ad6963847955927
Micha de Groot <git@michadegroot.nl>
2018-09-11 17:37:50 +0200

    Added uniform mutation

--- src/model/mutation/UniformMutation.java
+++ src/model/mutation/UniformMutation.java
@@ -1,20 +1,16 @@
 package model.mutation;
 
 import model.Individual;
-import java.util.Random;
 import model.Genome;
 
+import java.util.Random;
 import java.util.List;
 
-public class GaussianMutation implements MutationInterface {
+public class UniformMutation implements MutationInterface {
 
     /**
-     * Mutation function for mutating children with Gaussian.
-     * Mutate all children with zero mean Gaussian
-     * It might be possible to change the variance of the Gaussian
-     * if we want to, but passing the desired variance requires a
-     * different function header or that it is saved in the Individual
-     * class or something like that.
+     * Mutation function for mutating children with uniform distribution.
+     *
      */
     @Override
     public List<Individual> doMutation(List<Individual> children) {
@@ -22,18 +18,20 @@ public class GaussianMutation implements MutationInterface {
         int genome_size;
         Genome mutated_genome;
         double genome_array[];
+        double min = -5.0;
+        double max = 5.0;
         Random r = new Random();
 
         for (int i = 0; i < nr_children; i++) {
             mutated_genome = children.get(i).getGenome();
             genome_size = mutated_genome.getSize();
-            genome_array = mutated_genome.getGenome();
-
+            genome_array = new double[genome_size];
             for (int j = 0; j < genome_size; j++) {
-                genome_array[j] += r.nextGaussian(); // Can be done with mean and variance
+                genome_array[j] = r.nextDouble() * (max - min) + max;
             }
             mutated_genome.setGenome(genome_array);
         }
+
         return children;
     }
 }
