f79b68d67dd54aac643b29d6b2a348228a1ec2ac
Maximilian Filtenborg <max.filtenborg@nextpax.com>
2018-09-11 12:55:28 +0200

    Update compile

--- src/compile.sh
+++ src/compile.sh
@@ -1,10 +1,13 @@
-javac -cp .:contest.jar player50.java
-jar cmf MainClass.txt submission.jar player50.class
-'=======
-cd model
-find -name "*.java" > ../sources.txt
-cd ..
-python compile.py
-javac -cp contest.jar @sources.txt
-javac -cp contest.jar player50.java
-'
+#!/usr/bin/env bash
+
+javac -cp .:contest.jar player50.java && jar cmf MainClass.txt submission.jar player50.class model/Population.class && java -jar testrun.jar -submission=player50 -evaluation=SphereEvaluation -seed=1
+
+#javac -cp .:classes.jar:contest.jar player50.java
+#jar cmf MainClass.txt submission.jar @sources.txt
+
+#cd model
+#find -name "*.java" > ../sources.txt
+#cd ..
+#python compile.py
+#javac -cp contest.jar @sources.txt
+#javac -cp contest.jar player50.java
