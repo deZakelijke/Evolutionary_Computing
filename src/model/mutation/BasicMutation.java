b7feffc664436ff9193d6c08539de3dee9360fa7
Micha de Groot <git@michadegroot.nl>
2018-09-11 15:14:58 +0200

    Started on mutation

--- src/model/mutation/BasicMutation.java
+++ src/model/mutation/BasicMutation.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 public class BasicMutation implements MutationInterface {
 
+    /**
+     * Mutation function for mutating children.
+     * Two options: uniform and Gaussian
+     */
     @Override
     public List<Individual> doMutation(List<Individual> children) {
         throw new NotImplementedException();
