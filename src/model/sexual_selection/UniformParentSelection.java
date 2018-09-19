bf783187388e2f692f5e2501d3676e83dda0ae8c
Micha de Groot <git@michadegroot.nl>
2018-09-19 14:50:52 +0200

    Finished uniform parent selection

--- src/model/sexual_selection/UniformParentSelection.java
+++ src/model/sexual_selection/UniformParentSelection.java
@@ -12,7 +12,7 @@ public class UniformParentSelection extends EmptyParentSelection implements Pare
 
     /**
      * init / constructor
-     *this.nr_couples = nr_couples;
+     *
      * @param nr_parents
      * @param nr_couples
      */
@@ -27,21 +27,18 @@ public class UniformParentSelection extends EmptyParentSelection implements Pare
     public List<Individual[]> select(List<Individual> populationList) {
         int length = populationList.size();
         Random r = new Random();
-
         if (length < getNr_parents()) return null;
 
-        for (int i = length - 1; i >= length - getNr_parents(); --i) {
-            Collections.swap(populationList, i, r.nextInt(i + 1));
+
+        List<Individual[]> parents = new List<Individual[]>;
+        for (int i = 0 ; i < this.nr_couples; i++) {
+            Individual[] new_couple = new Individual[this.nr_parents];
+            for (int j = 0; j < this.nr_parents; j++) {
+                new_couple[j] = populatinListget(r.nextInt(length));
+            }
+            parents.add(new_couple);
         }
-
-
-
-        //todo: per couple een Individual[] maken van twee ouders zodat we weten welke bij elkaar horen
-
-        throw new NotImplementedException();
-
-//        return populationList.subList(length - getNr_parents(), length);
-
+        return parents;
         // Bereken totale fitness score, prob. voor selectie is f/Sf
         // Zorgt mestal voor te snelle convergence
         // Windowing toepassen: min(f) van elke f afhalen
