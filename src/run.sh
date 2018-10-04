daccf337580de4f6d99754185fe1425e5a39136b
Maximilian Filtenborg <max.filtenborg@nextpax.com>
2018-10-04 14:52:18 +0200

    Run all configurations

--- src/run.sh
+++ src/run.sh
@@ -8,8 +8,7 @@ CIGAR="./log/bent_cigar.txt"
 SCHAFFERS="./log/chaffers.txt"
 KATSUURA="./log/katsura.txt"
 
-#declare -a ps=("uniform" "deterministic_fitness" "ranking_parent" "stochastic_parent" "stochastic_parent_no_repitition" "tournament_parent")
-declare -a ps=("uniform" "deterministic_fitness" "ranking_parent" "tournament_parent")
+declare -a ps=("uniform" "deterministic_fitness" "ranking_parent" "stochastic_parent" "stochastic_parent_no_repitition" "tournament_parent")
 declare -a rc=("uniform" "simple_arithmetic")
 
 for i in {2..10}
@@ -27,13 +26,13 @@ do
                echo "Computing with params:" $parent_scheme $recomb_scheme $parents
 
                echo "cigar"
-               java -Dparent_scheme=${parent_scheme} -Drecomb_scheme=${recomb_scheme} -Dparents=${parents} -jar testrun.jar -submission=player50 -evaluation=BentCigarFunction -seed=1 1>> "$CIGAR" 2> /dev/null
+#               java -Dparent_scheme=${parent_scheme} -Drecomb_scheme=${recomb_scheme} -Dparents=${parents} -jar testrun.jar -submission=player50 -evaluation=BentCigarFunction -seed=1 1>> "$CIGAR" 2> ./log/error.txt
 
                echo "schaffers"
-               java -Dparent_scheme=${parent_scheme} -Drecomb_scheme=${recomb_scheme} -Dparents=${parents} -jar testrun.jar -submission=player50 -evaluation=SchaffersEvaluation -seed=1 1>> "$SCHAFFERS" 2> /dev/null
+#               java -Dparent_scheme=${parent_scheme} -Drecomb_scheme=${recomb_scheme} -Dparents=${parents} -jar testrun.jar -submission=player50 -evaluation=SchaffersEvaluation -seed=1 1>> "$SCHAFFERS" 2> /dev/null
 
                echo "katsuura"
-               java -Dparent_scheme=${parent_scheme} -Drecomb_scheme=${recomb_scheme} -Dparents=${parents} -jar testrun.jar -submission=player50 -evaluation=KatsuuraEvaluation -seed=1 1>> "$KATSUURA" 2> /dev/null
+               java -Dparent_scheme=${parent_scheme} -Drecomb_scheme=${recomb_scheme} -Dparents=${parents} -jar testrun.jar -submission=player50 -evaluation=KatsuuraEvaluation -seed=1 1>> "$KATSUURA" 2> ./log/error_kats.txt
            done
         done
     done
